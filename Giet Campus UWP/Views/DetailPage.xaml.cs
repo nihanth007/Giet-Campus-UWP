db28a8e6cc24635513c0fd84c605f85623fb9460
Nihanth Charan Mutluru <nihanthmbc@outlook.com>
2017-02-15 03:24:03 +0530

    Add project files.

---
+++ Giet Campus UWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Giet_Campus_UWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Giet_Campus_UWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
